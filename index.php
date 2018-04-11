8b56b123ab266a2da46477c6680b467f9235eed6
Niclas <niclas.sv@gmail.com>
2018-04-11 19:09:57 +0200

    added guzzleclient

--- index.php
+++ index.php
@@ -6,12 +6,25 @@
     <?php
     require 'vendor/autoload.php';
 
-    use Monolog\Logger;
-    use Monolog\Handler\StreamHandler;
+    use \Monolog\Logger;
+    use \Monolog\Handler\StreamHandler;
+    use GuzzleHttp\Client;
 
-    $log = new Logger('Test');
+    $log = new Logger('SiteVisit');
     $log->pushHandler(new StreamHandler('test.log', Logger::INFO));
-    $log->info('Site accessed');
+    // $log->info('Page (index.php) accessed');
+
+    $client = new Client([
+        'headers' => [
+            'accept' => ['application/json']
+        ]
+    ]);
+    $res = $client->request('GET', 'http://unicorns.idioti.se/');
+    $data = json_decode($res->getBody());
+    foreach($data as $unicorn) {
+        echo '<h1>' . $unicorn -> name . '</h1>';
+    }
     ?>
+
   </body>
 </html>
